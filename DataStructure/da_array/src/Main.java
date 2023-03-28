ae2f47e958a64963843d3a3e259b776473a0a552
97tkddnjs <tkddnjs206@gmail.com>
2023-03-28 19:59:58 +0900

    dd

---
+++ DataStructure/da_array/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] a = new int[10];
+        Integer[] b = new Integer[5];
+
+        System.out.println("Hello world!");
+    }
+}
