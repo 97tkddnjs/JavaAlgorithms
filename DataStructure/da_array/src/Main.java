2119f26ae3fb0f1152140bcbd940c6ba69f0c26b
Lee Sang Won <tkddnjs206@gmail.com>
2023-03-28 23:40:10 +0900

    chapter2

--- DataStructure/da_array/src/Main.java
+++ DataStructure/da_array/src/Main.java
@@ -1,9 +1,40 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        int[] a = new int[10];
-        Integer[] b = new Integer[5];
+        Random random = new Random();
 
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        
+        int num = sc.nextInt();
+        
+        
+        //int[] a = new int[10];
+        //Integer[] b = new Integer[5];
+
+        int[] height = new int[num];
+        System.out.println("height is that");
+
+        for (int i = 0; i < height.length; i++) {
+            height[i] = 100+ random.nextInt(90); // random에 바운드를 준다.
+            System.out.println("height[i] = " + height[i]);
+        }
+        System.out.println(Arrays.stream(height).max());
+
+        int max_idx = height.length-1;
+        for(int i = 0; i< (height.length)/2 ; i++ ){
+            int tmp =  height[i];
+            height[i] = height[max_idx-i];
+            height[max_idx-i] = tmp;
+        }
+        System.out.println("reverse");
+        for (int i = 0; i < height.length; i++) {
+            //height[i] = 100+ random.nextInt(90); // random에 바운드를 준다.
+            System.out.println("height[i] = " + height[i]);
+        }
     }
 }
---
+++ DataStructure/da_array/src/chapter2/Chapter2.java
@@ -0,0 +1,37 @@
+package chapter2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Chapter2 {
+    
+    // 동적 배열을 통해 radix number을 구현하고자 함
+    public  static ArrayList<String> radixNum(int input , int r){
+        String data = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int tmp;
+        ArrayList<String> chlist = new ArrayList<String>();
+        System.out.println("input = " + input + " r "+r +"  op "+input%r);
+        do{
+            tmp = input%r;
+            System.out.println("tmp = " + tmp);
+            chlist.add(String.valueOf(data.charAt(tmp)));
+            input = input/r;
+        }while(input != 0);
+
+        Collections.reverse(chlist);
+        return chlist;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        int radix = sc.nextInt();
+
+        ArrayList<String> strings = radixNum(num, radix);
+        strings.forEach(data ->System.out.print(data));
+
+
+    }
+}
